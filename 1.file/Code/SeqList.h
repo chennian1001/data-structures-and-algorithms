8aec25692f3eee1a850e7e0cdc431f9ab862a142
chennian <1529302058@qq.com>
2022-06-14 20:08:41 +0800

    master

--- 1.file/Code/SeqList.h
+++ 1.file/Code/SeqList.h
@@ -1,11 +1,18 @@
 #pragma once
+#define N 1000
+typedef int SLDataType;
 
-#define N 100;
-
-//静态数据
-struct SeqList
+//静态顺序表
+typedef struct SeqList
 {
-    int a[N];
-    int size; //表示数组中存储了多少个有效数组
+    SLDataType a[N];
+    int size;
+}SL;
 
-}
+//接口函数
+void SeqListInit(SL* ps,SLDataType x);
+void SeqListPushBack(SL* ps,SLDataType x);
+void SeqListPopBack(SL* ps,SLDataType x);
+void SeqListPushFront(SL* ps,SLDataType x);
+void SeqListPopFront(SL* ps,SLDataType x);
+//...
